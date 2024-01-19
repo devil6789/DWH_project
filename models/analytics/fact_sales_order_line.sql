4a958fd0d560cf4de0b6e7964738dae8db998e2c
kivisa <noinung4@gmail.com>
2024-01-19 13:29:40 +0700

    tach lop

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,11 +1,46 @@
-SELECT 
-  cast(order_line_id as integer) as	sales_order_line_key
-  , cast(stock_item_id as integer) as product_key
-, cast(quantity as integer) as quantity
-, cast(unit_price as numeric) as unit_price
-,  cast(quantity as integer) * cast(unit_price as numeric) as gross_amount
+with fact_sales_order_line__source AS (
+  SELECT * from `vit-lam-data.wide_world_importers.sales__order_lines` 
+)
 
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+, fact_sales_order_line__rename_column AS (
+  SELECT
+      order_line_id as	sales_order_line_key
+    , stock_item_id  as product_key
+    , quantity  as quantity
+    , unit_price as unit_price
+   FROM `fact_sales_order_line__source`
+)
+
+, fact_sales_order_line__cast_type AS (
+  SELECT 
+      cast(sales_order_line_key as int)  as	sales_order_line_key
+      , cast(product_key as int ) as product_key
+      , cast(quantity as integer) as quantity
+      , cast(unit_price as numeric) as unit_price
+  FROM `fact_sales_order_line__rename_column`
+)
+
+, fact_sales_order_line__add_gross_amount AS (
+  SELECT 
+      sales_order_line_key
+      , product_key
+      , quantity
+      , unit_price
+      , quantity * unit_price as gross_amount
+  FROM `fact_sales_order_line__cast_type`     
+)
+
+
+
+
+
+SELECT 
+    sales_order_line_key
+    , product_key
+    , quantity
+    , unit_price
+    , gross_amount
+FROM `fact_sales_order_line__add_gross_amount`
 
 
 
