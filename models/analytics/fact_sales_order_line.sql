38db499a5397e14cab867a2da3f5fc53bc885fc1
kivisa <noinung4@gmail.com>
2024-01-20 14:50:28 +0700

    add sales_order_key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -5,6 +5,7 @@ with fact_sales_order_line__source AS (
 , fact_sales_order_line__rename_column AS (
   SELECT
       order_line_id as	sales_order_line_key
+     , order_id as sales_orders_key
     , stock_item_id  as product_key
     , quantity  as quantity
     , unit_price as unit_price
@@ -14,15 +15,17 @@ with fact_sales_order_line__source AS (
 , fact_sales_order_line__cast_type AS (
   SELECT 
       cast(sales_order_line_key as int)  as	sales_order_line_key
+       , cast(sales_orders_key AS int)  as sales_orders_key
       , cast(product_key as int ) as product_key
       , cast(quantity as integer) as quantity
       , cast(unit_price as numeric) as unit_price
   FROM `fact_sales_order_line__rename_column`
 )
 
-, fact_sales_order_line__add_gross_amount AS (
+, fact_sales_order_line__calculate AS (
   SELECT 
       sales_order_line_key
+       , sales_orders_key
       , product_key
       , quantity
       , unit_price
@@ -36,11 +39,14 @@ with fact_sales_order_line__source AS (
 
 SELECT 
     sales_order_line_key
+     , sales_orders_key 
     , product_key
     , quantity
     , unit_price
     , gross_amount
-FROM `fact_sales_order_line__add_gross_amount`
+FROM `fact_sales_order_line__calculate`
+
+
 
 
 
