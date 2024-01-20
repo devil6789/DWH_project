1f75e8d7a9a2dec81dca948376d636899d9d95c2
kivisa <noinung4@gmail.com>
2024-01-21 06:42:07 +0700

    tach lop xu li

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,4 +1,20 @@
-SELECT 
-  supplier_id as supplier_key
-  ,supplier_name as	supplier_name
-FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+with dim_supplier__source AS (
+  SELECT * FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+, dim_supplier__rename AS (
+  SELECT supplier_id AS	supplier_key
+         , supplier_name
+  FROM `dim_supplier__source`       
+)
+
+, dim_supplier__cast_type AS (
+  SELECT
+    cast(supplier_key AS int) as supplier_key
+    , cast(supplier_name as string) AS supplier_name
+  FROM `dim_supplier__rename`  
+)
+
+SELECT supplier_key
+       , supplier_name
+FROM `dim_supplier__cast_type`
