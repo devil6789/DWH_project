57389c899f223c1faa697e8aa8d5958c8df51f20
kivisa <noinung4@gmail.com>
2024-01-20 18:34:43 +0700

    add customer_key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -5,7 +5,7 @@ with fact_sales_order_line__source AS (
 , fact_sales_order_line__rename_column AS (
   SELECT
       order_line_id as	sales_order_line_key
-     , order_id as sales_orders_key
+     , order_id as sales_order_key
     , stock_item_id  as product_key
     , quantity  as quantity
     , unit_price as unit_price
@@ -15,7 +15,7 @@ with fact_sales_order_line__source AS (
 , fact_sales_order_line__cast_type AS (
   SELECT 
       cast(sales_order_line_key as int)  as	sales_order_line_key
-       , cast(sales_orders_key AS int)  as sales_orders_key
+       , cast(sales_order_key AS int)  as sales_order_key
       , cast(product_key as int ) as product_key
       , cast(quantity as integer) as quantity
       , cast(unit_price as numeric) as unit_price
@@ -25,7 +25,7 @@ with fact_sales_order_line__source AS (
 , fact_sales_order_line__calculate AS (
   SELECT 
       sales_order_line_key
-       , sales_orders_key
+       , sales_order_key
       , product_key
       , quantity
       , unit_price
@@ -34,17 +34,17 @@ with fact_sales_order_line__source AS (
 )
 
 
-
-
-
 SELECT 
-    sales_order_line_key
-     , sales_orders_key 
-    , product_key
-    , quantity
-    , unit_price
-    , gross_amount
-FROM `fact_sales_order_line__calculate`
+   fact_line.sales_order_line_key
+    , fact_line.sales_order_key 
+    , fact_line.product_key
+    , fact_header.customer_key
+    , fact_line.quantity
+    , fact_line.unit_price
+    , fact_line.gross_amount
+FROM `fact_sales_order_line__calculate` as fact_line
+left join `learn-dwh-411512.wide_world_importers_dwh_staging.stg_fact_sales_order` as fact_header
+on fact_line.sales_order_key = fact_header.sales_order_key
 
 
 
