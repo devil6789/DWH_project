1ac4542224fc6f05d4d502161d4c640e07636203
kivisa <noinung4@gmail.com>
2024-01-20 14:07:10 +0700

    tach lop xu li

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,4 +1,18 @@
-SELECT 
-  customer_id as 	customer_key
-  ,customer_name	as customer_name
+with dim_customer__source AS (
+  SELECT 
+  *
 FROM `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+, dim_customer__rename AS (
+  SELECT customer_id AS	customer_key
+        ,customer_name AS	customer_name
+  FROM `dim_customer__source`       
+)
+
+, dim_customer__cast AS (
+  select cast(customer_key  AS int) as customer_key
+        , cast(customer_name as string) as customer_name
+  FROM `dim_customer__rename`
+)
+SELECT * from `dim_customer__cast`
