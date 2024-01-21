887facd51ae68c90c0fa043fe56db837d39f63ac
kivisa <noinung4@gmail.com>
2024-01-21 07:47:03 +0700

    add customer_category_key, buying_group_key

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -7,14 +7,20 @@ FROM `vit-lam-data.wide_world_importers.sales__customers`
 , dim_customer__rename AS (
   SELECT customer_id AS	customer_key
         ,customer_name AS	customer_name
+        , customer_category_id as customer_category_key
+        , buying_group_id AS buying_group_key
   FROM `dim_customer__source`       
 )
 
 , dim_customer__cast AS (
   select cast(customer_key  AS int) as customer_key
         , cast(customer_name as string) as customer_name
+        , cast(customer_category_key as int) as customer_category_key
+        , cast(buying_group_key as int) as buying_group_key 
   FROM `dim_customer__rename`
 )
-SELECT customer_key
-      , customer_name
- from `dim_customer__cast`
+SELECT dim_customer.customer_key
+      , dim_customer.customer_name
+      , dim_customer.customer_category_key
+      , dim_customer.buying_group_key
+ from `dim_customer__cast` as dim_customer
