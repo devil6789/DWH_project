3dc928f14ab262cacb6a2036a08552e0ee0b9915
kivisa <noinung4@gmail.com>
2024-01-24 10:06:31 +0700

    handle boolean to string

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -9,7 +9,7 @@ with dim_product__source AS  (
     , stock_item_name  as	product_name
     , supplier_id as supplier_key
     , brand  as brand_name
-    , is_chiller_stock
+    , is_chiller_stock as is_chiller_stock_boolean
 FROM `dim_product__source`
     )
 
@@ -20,9 +20,20 @@ FROM `dim_product__source`
         , cast(product_name as STRING) as	product_name
         , cast(supplier_key as INT) as supplier_key
         , cast(brand_name AS STRING) as brand_name
-        , cast(is_chiller_stock as BOOLEAN) as is_chiller_stock
+        , cast(is_chiller_stock_boolean as BOOLEAN) as is_chiller_stock_boolean
       FROM `dim_product__rename_column`  
     ) 
+
+, dim_product__handle_boolean AS (
+  SELECT *,
+      CASE 
+      when is_chiller_stock_boolean is TRUE then 'Chiller Stock'
+      when is_chiller_stock_boolean is FALSE then 'Not Chiller Stock'
+      when is_chiller_stock_boolean is NULL then 'Undefined'
+      else 'Invalid'
+      end AS is_chiller_stock
+  FROM `dim_product__casted`
+)
 
 SELECT dim_product.product_key
       , dim_product.product_name
@@ -30,7 +41,7 @@ SELECT dim_product.product_key
       , dim_supplier.supplier_name
       , dim_product.brand_name
       , dim_product.is_chiller_stock
-from dim_product__casted as dim_product
+from dim_product__handle_boolean as dim_product
 left join {{ ref('dim_supplier') }} as dim_supplier
 on dim_product.supplier_key = dim_supplier.supplier_key
 
