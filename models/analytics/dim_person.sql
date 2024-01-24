17e1066e62c539a3c1f59d72b214659648a722a0
kivisa <noinung4@gmail.com>
2024-01-24 11:01:05 +0700

    add person_key and full_name

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,23 @@
-SELECT 
+WITH dim_person__source AS (
+  SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.application__people`
+)
+
+, dim_person__rename AS (
+  SELECT person_id AS	person_key
+         , full_name
+  FROM `dim_person__source`
+)
+
+, dim_person__cast_type AS (
+  SELECT cast(person_key as int) as person_key
+         , cast(full_name as STRING) AS full_name
+  FROM `dim_person__rename`
+)
+
+SELECT  
+    person_key
+    , full_name
+FROM `dim_person__cast_type`
+
