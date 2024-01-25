135b62a73dd6ae56dc8cfd67a38ad8916a6e2852
kivisa <noinung4@gmail.com>
2024-01-25 13:18:56 +0700

    khu NULL cot customer_key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -38,7 +38,7 @@ SELECT
       fact_line.sales_order_line_key
     , fact_line.sales_order_key 
     , fact_line.product_key
-    , fact_header.customer_key
+    , coalesce(fact_header.customer_key, -1) as customer_key
     , fact_line.quantity
     , fact_line.unit_price
     , fact_line.gross_amount
