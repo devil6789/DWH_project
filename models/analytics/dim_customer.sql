b400d6d88dd0de7eb41d939aeb5ee57dc0587151
kivisa <noinung4@gmail.com>
2024-01-31 21:47:09 +0700

    handle boolean

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,63 +1,56 @@
-with dim_customer__source AS (
-  SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+WITH dim_customer__source AS (
+    SELECT *
+    FROM `vit-lam-data.wide_world_importers.sales__customers`
 )
 
 , dim_customer__rename AS (
-  SELECT customer_id AS	customer_key
-        ,customer_name AS	customer_name
-        , customer_category_id as customer_category_key
-        , buying_group_id AS buying_group_key
-        , is_on_credit_hold as is_on_credit_hold_boolean
-  FROM `dim_customer__source`       
+    SELECT
+        customer_id AS customer_key
+        , customer_name
+        , is_statement_sent AS is_statement_sent_boolean
+        , is_on_credit_hold AS is_on_credit_hold_boolean
+        , standard_discount_percentage
+        , payment_days AS customer_payment_days
+        , credit_limit
+        , account_opened_date
+    FROM `dim_customer__source`
 )
 
-, dim_customer__cast AS (
-  select cast(customer_key  AS int) as customer_key
-        , cast(customer_name as string) as customer_name
-        , cast(customer_category_key as int) as customer_category_key
-        , cast(buying_group_key as int) as buying_group_key
-        , cast(is_on_credit_hold_boolean AS BOOLEAN) as is_on_credit_hold_boolean
-  FROM `dim_customer__rename`
+, dim_customer__cast_type AS (
+    SELECT
+        CAST(customer_key AS INT) AS customer_key
+        , CAST(customer_name AS STRING) AS customer_name
+        , CAST(is_statement_sent_boolean AS BOOLEAN) AS is_statement_sent_boolean
+        , CAST(is_on_credit_hold_boolean AS BOOLEAN) AS is_on_credit_hold_boolean
+        , CAST(standard_discount_percentage AS NUMERIC) AS standard_discount_percentage
+        , CAST(customer_payment_days AS INT) AS customer_payment_days 
+        , CAST(credit_limit AS NUMERIC) AS credit_limit
+        , CAST(account_opened_date AS DATE) AS account_opened_date         
+    FROM `dim_customer__rename`
 )
 
 , dim_customer__handle_boolean AS (
-  SELECT *
+    SELECT
+        customer_key
+        , customer_name
         , CASE
-            WHEN is_on_credit_hold_boolean is TRUE then 'On Creadit Hold '
-            WHEN is_on_credit_hold_boolean is FALSE then 'Not On Creadit Hold '
-            WHEN is_on_credit_hold_boolean is NULL then 'Undefined'
-            else 'Invalid'
-          END as is_on_credit_hold
-  FROM `dim_customer__cast`
+            WHEN is_statement_sent_boolean IS TRUE THEN 'Statement Sent'
+            WHEN is_statement_sent_boolean IS FALSE THEN 'Not Statement Sent'
+            WHEN is_statement_sent_boolean IS NULL THEN 'Undefined'
+            ELSE 'Invalid'
+          END AS is_statement_sent
+        , CASE
+            WHEN is_on_credit_hold_boolean IS TRUE THEN 'On Credit Hold'
+            WHEN is_on_credit_hold_boolean IS FALSE THEN 'Not On Credit Hold'
+            WHEN is_on_credit_hold_boolean IS NULL THEN 'Undefined'
+            ELSE 'Invalid'
+          END AS is_on_credit_hold 
+        , standard_discount_percentage
+        , customer_payment_days
+        , credit_limit
+        , account_opened_date
+    FROM `dim_customer__cast_type`
 )
 
-, dim_customer__add_row_undefined_invald AS (
-SELECT dim_customer.customer_key
-      , dim_customer.customer_name
-      , dim_customer.customer_category_key
-      , coalesce(stg_dim_category.customer_category_name, 'Undefined') AS customer_category_name 
-      , dim_customer.buying_group_key
-      , coalesce(stg_dim_buying_group.buying_group_name, 'Undefined') AS buying_group_name 
-      , dim_customer.is_on_credit_hold
- from `dim_customer__handle_boolean` as dim_customer
-  LEFT JOIN {{ ref('stg_dim_customer_category') }} as stg_dim_category
-    ON dim_customer.customer_category_key = stg_dim_category.customer_category_key
-  LEFT JOIN {{ ref("stg_dim_buying_group") }} as stg_dim_buying_group
-    ON dim_customer.buying_group_key = stg_dim_buying_group.buying_group_key
-Union ALL
-SELECT 0 as customer_key, 'Undefined' as customer_category_name, 0 as customer_category_key, 'Undefined' as customer_category_name, NULL as buying_group_key, 'Undefined' as buying_group_name, 'Undefined' as is_on_credit_hold
-Union ALL 
-SELECT -1 as customer_key, 'Invalid' as customer_category_name, -1 as customer_category_key, 'Invalid' as customer_category_name, -1 as buying_group_key, 'Invalid' as buying_group_name, 'Invalid' as is_on_credit_hold
-)
-
-SELECT 
-      customer_key
-      , customer_name
-      , is_on_credit_hold
-      , customer_category_key
-      , customer_category_name
-      , coalesce(buying_group_key, 0) as buying_group_key
-      , buying_group_name     
-FROM `dim_customer__add_row_undefined_invald`
+    SELECT *
+    FROM `dim_customer__handle_boolean`
