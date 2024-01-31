1e43d9fc5d7dccbd7881df32c4f10706497fe1a3
kivisa <noinung4@gmail.com>
2024-01-31 20:04:22 +0700

    flatten supplier_category to supplier

--- models/staging/stg_dim_supplier.sql
+++ models/staging/stg_dim_supplier.sql
@@ -8,6 +8,7 @@ WITH dim_supplier__source AS (
         supplier_id as supplier_key
         , supplier_name
         , payment_days as supplier_payment_days
+        , supplier_category_id as supplier_category_key
     FROM `dim_supplier__source`
 )
 
@@ -16,8 +17,17 @@ WITH dim_supplier__source AS (
         CAST(supplier_key AS INT) AS supplier_key
         , CAST(supplier_name AS STRING) AS supplier_name
         , CAST(supplier_payment_days AS INT) AS supplier_payment_days
+        , CAST(supplier_category_key AS INT) AS supplier_category_key
+
     FROM `dim_supplier__rename`
 )
 
-    SELECT *
-    FROM `dim_supplier__cast_type`
+    SELECT 
+        dim_supplier.supplier_key
+        , dim_supplier.supplier_name
+        , dim_supplier.supplier_payment_days
+        , dim_supplier.supplier_category_key
+        , dim_supplier_category.supplier_category_name
+    FROM `dim_supplier__cast_type` AS dim_supplier
+    LEFT JOIN {{ ref("stg_dim_supplier_category") }} as dim_supplier_category
+    ON dim_supplier.supplier_category_key = dim_supplier_category.supplier_category_key
