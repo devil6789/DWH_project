deba34fa53667b6f7761ba486ff53956c186ab92
kivisa <noinung4@gmail.com>
2024-01-31 22:02:44 +0700

    tao bang

---
+++ models/staging/stg_dim_customer_buying_group.sql
@@ -0,0 +1,25 @@
+WITH stg_dim_customer_buying_group__source AS (
+    SELECT *
+    FROM `vit-lam-data.wide_world_importers.sales__buying_groups`
+)
+
+, stg_dim_customer_buying_group__rename AS (
+    SELECT
+        buying_group_id AS buying_group_key
+        , buying_group_name
+    FROM `stg_dim_customer_buying_group__source`
+)
+
+, stg_dim_customer_buying_group__cast_type AS (
+    SELECT
+        CAST(buying_group_key AS INT) AS buying_group_key
+        , CAST(buying_group_name AS STRING) AS buying_group_name
+    FROM `stg_dim_customer_buying_group__rename`
+)
+
+
+
+    SELECT
+        buying_group_key
+        , buying_group_name
+    FROM `stg_dim_customer_buying_group__cast_type`
