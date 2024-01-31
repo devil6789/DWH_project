086fcc23716048bf9020bfd643019fae7426a8b5
kivisa <noinung4@gmail.com>
2024-01-31 20:25:14 +0700

    flatten supplier to product

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -13,6 +13,8 @@ WITH dim_product__source AS (
         , tax_rate
         , unit_price
         , recommended_retail_price
+        , lead_time_days
+        , supplier_id AS supplier_key
     FROM `dim_product__source`
 )
 
@@ -25,8 +27,29 @@ WITH dim_product__source AS (
         , CAST(quantity_per_outer AS INT) AS quantity_per_outer 
         , CAST(tax_rate AS NUMERIC) AS tax_rate
         , CAST(unit_price AS NUMERIC) AS unit_price
-        , CAST(recommended_retail_price AS NUMERIC) AS recommended_retail_price  
+        , CAST(recommended_retail_price AS NUMERIC) AS recommended_retail_price
+        , CAST(lead_time_days AS INT) AS lead_time_days 
+        , CAST(supplier_key AS INT) AS supplier_key
     FROM `dim_product__rename`    
 )
 
-SELECT * FROM dim_product__cast_type
+    SELECT
+        dim_product.product_key
+        , dim_product.product_name
+        , dim_product.brand_name
+        , dim_product.product_size
+        , dim_product.quantity_per_outer
+        , dim_product.tax_rate
+        , dim_product.unit_price
+        , dim_product.recommended_retail_price
+        , dim_product.lead_time_days
+        , dim_product.supplier_key
+        , dim_supplier.supplier_name
+        , dim_supplier.supplier_payment_days
+        , dim_supplier.supplier_category_key
+        , dim_supplier.supplier_category_name
+        , dim_supplier.delivery_method_key
+        , dim_supplier.delivery_method_name
+    FROM `dim_product__cast_type` AS dim_product
+      LEFT JOIN {{ ref("stg_dim_supplier") }} AS dim_supplier
+        ON dim_product.supplier_key = dim_supplier.supplier_key
