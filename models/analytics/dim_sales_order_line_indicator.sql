c19f947b5160c0f37cba1917809cc5078c9f6050
kivisa <noinung4@gmail.com>
2024-02-06 19:33:18 +0700

    them dau phay

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -1,10 +1,12 @@
-with dim_is_undersupply_backorder AS (
-    SELECT 1 AS is_undersupply_backorder_boolean,'Undersupply Backorder' AS is_undersupply_backorder
+with dim_is_undersupply_backordered__source AS (
+    SELECT 'TRUE' AS is_undersupply_backordered_boolean,'Undersupply Backorder' AS is_undersupply_backordered
     UNION ALL 
-    SELECT 2 AS is_undersupply_backorder_boolean,'Not Undersupply Backorder' AS is_undersupply_backorder
+    SELECT 'FALSE' AS is_undersupply_backordered_boolean,'Not Undersupply Backorder' AS is_undersupply_backordered
 )
 
-    SELECT *
+    SELECT 
+        concat(package_type_key, ',', is_undersupply_backordered_boolean) AS sales_order_line_indicator_key
+        , *
     FROM `learn-dwh-411512.wide_world_importers_dwh.dim_package_type`
-    CROSS JOIN dim_is_undersupply_backorder
+    CROSS JOIN dim_is_undersupply_backordered__source
     ORDER BY 3,1
