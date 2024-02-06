dadc5f87864c35eac2541a2a773f9fe08bf04b98
kivisa <noinung4@gmail.com>
2024-02-06 19:52:03 +0700

    thêm ed cho giá trị

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -1,7 +1,7 @@
 with dim_is_undersupply_backordered__source AS (
-    SELECT 'TRUE' AS is_undersupply_backordered_boolean,'Undersupply Backorder' AS is_undersupply_backordered
+    SELECT 'TRUE' AS is_undersupply_backordered_boolean,'Undersupply Backordered' AS is_undersupply_backordered
     UNION ALL 
-    SELECT 'FALSE' AS is_undersupply_backordered_boolean,'Not Undersupply Backorder' AS is_undersupply_backordered
+    SELECT 'FALSE' AS is_undersupply_backordered_boolean,'Not Undersupply Backordered' AS is_undersupply_backordered
 )
 
     SELECT 
@@ -9,4 +9,4 @@ with dim_is_undersupply_backordered__source AS (
         , *
     FROM `learn-dwh-411512.wide_world_importers_dwh.dim_package_type`
     CROSS JOIN dim_is_undersupply_backordered__source
-    ORDER BY 3,1
+    
