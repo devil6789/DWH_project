77c4b128284103f059b450f4255a3e2e9de81f6d
kivisa <noinung4@gmail.com>
2024-02-06 19:35:18 +0700

    ko dung cot boolean, dung cot string

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -5,7 +5,7 @@ with dim_is_undersupply_backordered__source AS (
 )
 
     SELECT 
-        concat(package_type_key, ',', is_undersupply_backordered_boolean) AS sales_order_line_indicator_key
+        concat(package_type_key, ',', is_undersupply_backordered) AS sales_order_line_indicator_key
         , *
     FROM `learn-dwh-411512.wide_world_importers_dwh.dim_package_type`
     CROSS JOIN dim_is_undersupply_backordered__source
