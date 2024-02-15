891f09c00080152e54e6dcd79aca6a251715669e
kivisa <noinung4@gmail.com>
2024-02-15 16:58:10 +0700

    goi ten tung cot ra

--- models/analytics/dim_sales_order_line_indicator.sql
+++ models/analytics/dim_sales_order_line_indicator.sql
@@ -6,7 +6,10 @@ with dim_is_undersupply_backordered__source AS (
 
     SELECT 
         FARM_FINGERPRINT(concat(dim_package_type.package_type_key, ',', dim_is_undersupply_backordered__source.is_undersupply_backordered)) AS sales_order_line_indicator_key
-        , *
+        , package_type_key	
+        , package_type_name	
+        , is_undersupply_backordered_boolean	
+        , is_undersupply_backordered
     FROM `learn-dwh-411512.wide_world_importers_dwh.dim_package_type` AS dim_package_type
     CROSS JOIN dim_is_undersupply_backordered__source
     
