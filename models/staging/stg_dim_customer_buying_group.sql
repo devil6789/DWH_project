722b5d3bc3462c2a481655eff045fd38228fb621
kivisa <noinung4@gmail.com>
2024-02-16 09:32:01 +0700

    add null and invalid value

--- models/staging/stg_dim_customer_buying_group.sql
+++ models/staging/stg_dim_customer_buying_group.sql
@@ -23,3 +23,8 @@ WITH stg_dim_customer_buying_group__source AS (
         buying_group_key
         , buying_group_name
     FROM `stg_dim_customer_buying_group__cast_type`
+    UNION ALL
+    SELECT 0, 'Undefined'
+
+    UNION ALL
+    SELECT -1, 'Invalid'
