6d9df81891869a137661b8b66de73d7aeb725dd8
kivisa <noinung4@gmail.com>
2024-02-17 15:08:41 +0700

    bang dim cho fact_purchase_order_line

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,29 +1,14 @@
-with dim_supplier__source AS (
-  SELECT * FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+WITH dim_supplier__source AS (
+    SELECT *
+    FROM {{ ref("stg_dim_supplier") }}
 )
 
-, dim_supplier__rename AS (
-  SELECT supplier_id AS	supplier_key
-         , supplier_name
-  FROM `dim_supplier__source`       
-)
-
-, dim_supplier__cast_type AS (
-  SELECT
-    cast(supplier_key AS int) as supplier_key
-    , cast(supplier_name as string) AS supplier_name
-  FROM `dim_supplier__rename`  
-)
-
-, dim_supplier__add_undefined_invalid_value AS (
-SELECT supplier_key
-       , supplier_name
-FROM `dim_supplier__cast_type`
-UNION ALL 
-select 0 as supplier_key, 'Undefined' AS supplier_name
-UNION ALL 
-select -1 as supplier_key, 'Invalid' AS supplier_name
-)
-SELECT supplier_key
-       , supplier_name 
-FROM `dim_supplier__add_undefined_invalid_value`
+    SELECT 
+        supplier_key
+        , supplier_name
+        , supplier_payment_days
+        , supplier_category_key
+        , supplier_category_name
+        , delivery_method_key
+        , delivery_method_name
+    FROM `dim_supplier__source`
