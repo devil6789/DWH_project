94eb2a4a7271c283e3b34bc9feb4950598e10712
kivisa <noinung4@gmail.com>
2024-02-17 11:02:24 +0700

    doi stock_item thanh product

--- models/analytics/fact_purchase_order_line.sql
+++ models/analytics/fact_purchase_order_line.sql
@@ -8,7 +8,7 @@ WITH fact_purchase_order_line__source AS (
         purchase_order_line_id AS purchase_order_line_key
         , description
         , purchase_order_id AS purchase_order_key
-        , stock_item_id AS stock_item_key
+        , stock_item_id AS product_key
         , package_type_id AS package_type_key
         , last_receipt_date
         , is_order_line_finalized AS is_order_line_finalized_boolean
@@ -23,7 +23,7 @@ WITH fact_purchase_order_line__source AS (
         CAST(purchase_order_line_key AS INT) AS purchase_order_line_key 
         , CAST(description AS STRING) AS description 
         , CAST(purchase_order_key AS INT) AS purchase_order_key 
-        , CAST(stock_item_key AS INT) AS stock_item_key 
+        , CAST(product_key AS INT) AS product_key 
         , CAST(package_type_key AS INT) AS package_type_key 
         , CAST(last_receipt_date AS DATE) AS last_receipt_date 
         , CAST(is_order_line_finalized_boolean AS BOOLEAN) AS is_order_line_finalized_boolean 
@@ -50,7 +50,7 @@ WITH fact_purchase_order_line__source AS (
         purchase_order_line_key
         , COALESCE(description, 'Undefined') AS description
         , purchase_order_key
-        , stock_item_key
+        , product_key
         , package_type_key
         , COALESCE(last_receipt_date, '2012-01-01') AS last_receipt_date
         , is_order_line_finalized_boolean
@@ -61,16 +61,28 @@ WITH fact_purchase_order_line__source AS (
     FROM `fact_purchase_order_line__handle_boolean`
 )
 
+, fact_purchase_order_line__join AS (
     SELECT
-        purchase_order_line_key
-        , description
-        , purchase_order_key
-        , stock_item_key
-        , package_type_key
-        , last_receipt_date
-        , is_order_line_finalized_boolean
-        , is_order_line_finalized
-        , ordered_outers
-        , received_outers
-        , expected_unit_price_per_outer
-    FROM `fact_purchase_order_line__handle_null`
+        purchase_order_line.purchase_order_line_key
+        , purchase_order_line.description
+        , purchase_order_line.purchase_order_key
+        , purchase_order.supplier_key
+        , purchase_order.delivery_method_key
+        , purchase_order.contact_person_key
+        , purchase_order_line.product_key
+        , purchase_order_line.package_type_key
+        , purchase_order_line.last_receipt_date
+        , purchase_order_line.is_order_line_finalized_boolean
+        , purchase_order_line.is_order_line_finalized
+        , purchase_order_line.ordered_outers
+        , purchase_order_line.received_outers
+        , purchase_order_line.expected_unit_price_per_outer
+    FROM `fact_purchase_order_line__handle_null` AS purchase_order_line
+      LEFT JOIN {{ ref("stg_fact_purchase_order") }} AS purchase_order
+        ON purchase_order.purchase_order_key = purchase_order_line.purchase_order_key
+)
+
+    SELECT *
+    FROM `fact_purchase_order_line__join`
+    
+
