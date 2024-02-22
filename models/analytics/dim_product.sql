35a6b3deedb7591f2fd00c1ee84ee1b234d6850f
kivisa <noinung4@gmail.com>
2024-02-22 15:51:41 +0700

    cu cast type xong la phai handle null luon

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -39,6 +39,24 @@ WITH dim_product__source AS (
     FROM `dim_product__rename`    
 )
 
+, dim_product__handle_null AS (
+    SELECT 
+        product_key
+        , COALESCE(product_name, 'Invalid') AS product_name 
+        , COALESCE(brand_name, 'Invalid') AS brand_name 
+        , COALESCE(product_size, 'Invalid') AS product_size 
+        , COALESCE(quantity_per_outer, 0) AS quantity_per_outer 
+        , COALESCE(tax_rate, 0) AS tax_rate 
+        , COALESCE(unit_price, 0) AS unit_price 
+        , COALESCE(recommended_retail_price, 0) AS recommended_retail_price 
+        , COALESCE(lead_time_days, 0) AS lead_time_days 
+        , COALESCE(supplier_key, 0) AS supplier_key 
+        , COALESCE(colour_key, 0) AS colour_key 
+        , COALESCE(unit_package_key, 0) AS unit_package_key 
+        , COALESCE(outer_package_key, 0) AS outer_package_key 
+    FROM `dim_product__cast_type`
+)
+
 , dim_product__join AS (
     SELECT
         dim_product.product_key
@@ -63,7 +81,7 @@ WITH dim_product__source AS (
         , COALESCE(dim_unit_package_type.package_type_name, 'Invalid') AS unit_package_type_name
         , dim_product.outer_package_key AS outer_package_type_key
         , COALESCE(dim_outer_package_type.package_type_name, 'Invalid') AS outer_package_type_name
-    FROM `dim_product__cast_type` AS dim_product
+    FROM `dim_product__handle_null` AS dim_product
       LEFT JOIN {{ ref("stg_dim_supplier") }} AS dim_supplier
         ON dim_product.supplier_key = dim_supplier.supplier_key
 
@@ -154,33 +172,6 @@ WITH dim_product__source AS (
         , 'Invalid'
 )
 
-, dim_product__handle_null AS (
-    SELECT 
-        product_key
-        , COALESCE(product_name, 'Undefined') AS product_name
-        , COALESCE(brand_name, 'Undefined') AS brand_name
-        , COALESCE(product_size, 'Undefined') AS product_size
-        , COALESCE(quantity_per_outer, 0) AS quantity_per_outer
-        , COALESCE(tax_rate, 0) AS tax_rate
-        , COALESCE(unit_price, 0) AS unit_price
-        , COALESCE(recommended_retail_price, 0) AS recommended_retail_price
-        , COALESCE(lead_time_days, 0) AS lead_time_days
-        , supplier_key
-        , COALESCE(supplier_name, 'Undefined') AS supplier_name
-        , COALESCE(supplier_payment_days, 0) AS supplier_payment_days
-        , supplier_category_key
-        , COALESCE(supplier_category_name, 'Undefined') AS supplier_category_name
-        , delivery_method_key
-        , COALESCE(delivery_method_name, 'Undefined') AS delivery_method_name
-        , colour_key
-        , COALESCE(colour_name, 'Undefined') AS colour_name
-        , unit_package_type_key
-        , COALESCE(unit_package_type_name, 'Undefined') AS unit_package_type_name
-        , outer_package_type_key
-        , COALESCE(outer_package_type_name, 'Undefined') AS outer_package_type_name
-    FROM `dim_product__add_undefined_invalid`
-)
-
     SELECT
         product_key
         , product_name
@@ -204,7 +195,7 @@ WITH dim_product__source AS (
         , unit_package_type_name
         , outer_package_type_key
         , outer_package_type_name
-    FROM `dim_product__handle_null`
+    FROM `dim_product__add_undefined_invalid`
 
     
     
