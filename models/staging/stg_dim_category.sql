5b0c1c5f0931c2f0a1b7249d8d646c6b660b0521
kivisa <noinung4@gmail.com>
2024-03-12 19:39:45 +0700

    add values undefined and invalid

--- models/staging/stg_dim_category.sql
+++ models/staging/stg_dim_category.sql
@@ -42,7 +42,7 @@ WITH dim_category__source AS (
     ON dim_category.parent_category_key = dim_category_parent.category_key
 )
 
-
+, dim_category__add_values_undefined_invalid AS (
     SELECT
         category_key
         , category_name
@@ -50,3 +50,28 @@ WITH dim_category__source AS (
         , parent_category_key
         , parent_category_name
     FROM `dim_category__join`
+
+    UNION ALL 
+    SELECT
+        0 AS category_key
+        , 'Undefined' AS category_name
+        , 0 AS category_level
+        , 0 AS parent_category_key
+        , 'Undefined' AS parent_category_name
+
+    UNION ALL 
+    SELECT
+        -1 AS category_key
+        , 'Invalid' AS category_name
+        , -1 AS category_level
+        , -1 AS parent_category_key
+        , 'Invalid' AS parent_category_name
+)
+
+    SELECT
+        category_key
+        , category_name
+        , category_level
+        , parent_category_key
+        , parent_category_name
+    FROM `dim_category__add_values_undefined_invalid`
